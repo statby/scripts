fc0ce792ab1f3c92a2e4556803e142665824ad20
Statby <zguicai@126.com>
2015-07-09 17:01:40 +0800

    增加获取天气脚本

--- gz_weather_final.py
+++ gz_weather_final.py
@@ -2,7 +2,6 @@
 #coding=utf-8
 import requests
 from bs4 import BeautifulSoup
-import random
 
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
