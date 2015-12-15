932db60953555315f7031d3283b0f5dd158fb962
staty <statby@gmail.com>
2015-12-15 10:58:17 +0800

    change structure tree

---
+++ py_scripts/python_time.py
@@ -0,0 +1,22 @@
+#!/bin/env python3
+#coding=utf-8
+
+import time,datetime
+
+x = 5
+
+now_str = datetime.datetime.now().strftime("%s")
+now_array = time.localtime(float(now_str))
+now_human = time.strftime("%Y%m%d %H:%M:%S",now_array)
+xminutes_ago_human = (datetime.datetime.now()-datetime.timedelta(minutes=int(x))).strftime("%Y%m%d %H:%M:%S")
+xminutes_ago_str = (datetime.datetime.now()-datetime.timedelta(minutes=int(x))).strftime("%s")
+
+
+
+
+print ("now_str=",now_str)
+print ("now_array=",now_array)
+print ("now_human=",now_human)
+
+print ("xminutes_ago_human=",xminutes_ago_human)
+print ("xminutes_ago_str=",xminutes_ago_str)
